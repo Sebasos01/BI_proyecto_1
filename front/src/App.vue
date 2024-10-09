1878af00596b08093d517b4a0d3ba6b109bd3647
Sebastian Ospino <sospino110@gmail.com>
2024-10-09 16:47:22 -0500

    fix2

---
+++ front/src/App.vue
@@ -0,0 +1,29 @@
+<!-- frontend/src/App.vue -->
+
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      message: 'Loading...'
+    };
+  },
+  mounted() {
+    axios.get('/api/')
+      .then(response => {
+        this.message = response.data.message;
+      })
+      .catch(error => {
+        console.log(error);
+        this.message = 'Error fetching data';
+      });
+  }
+};
+</script>
